ed0cd0545eb8aad9e798454552c4a89db43f0eeb
20951a05g1 <113289102+20951a05g1@users.noreply.github.com>
2023-03-10 23:35:11 +0530

    Create radixsort.py

---
+++ week2/radixsort.py
@@ -0,0 +1,36 @@
+def radix_sort(arr):
+    
+    max_num = max(arr)
+    
+    exp = 1
+    while max_num // exp > 0:
+        counting_sort(arr, exp)
+        exp *= 10
+
+def counting_sort(arr, exp):
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10
+
+    
+    for i in range(n):
+        index = arr[i] // exp
+        count[index % 10] += 1
+
+   
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    
+    for i in range(n - 1, -1, -1):
+        index = arr[i] // exp
+        output[count[index % 10] - 1] = arr[i]
+        count[index % 10] -= 1
+
+    
+    for i in range(n):
+        arr[i] = output[i]
+
+arr = [17, 45, 7, 0, 82, 24, 2, 56]
+radix_sort(arr)
+print(arr)  
